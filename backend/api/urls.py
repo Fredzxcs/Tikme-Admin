c4d2e9594b7138e3c516eadfb88bc4a6b99a22f9
Fredzxcs <johnfredericktsabondo@iskolarngbayan.pup.edu.ph>
2024-12-10 02:55:19 +0800

    v1

---
+++ backend/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    # create_reservation,
+    admin_dashboard,
+    event_reservation
+)
+
+
+urlpatterns = [
+    # Authentication
+    path('', admin_dashboard, name='admin_dashboard'),
+    # path('create_reservation/', create_reservation, name='create_reservation'),
+    path('event_reservation/', event_reservation, name='event_reservation'),
+    
+    
+]
